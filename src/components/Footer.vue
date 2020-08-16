e89b02ffce12da3c07d4fed357276f1695ea0683
Danijel <kosijer84@gmail.com>
2020-08-16 20:45:23 +0200

    components, finished homepage by design from pdf, mobile first

---
+++ src/components/Footer.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container-fluid footer text-center text-sm-left">
+    <div class="row">
+      <div class="col-xl-2 mb-3 mb-xl-0">
+        <small>© <span id="year"></span> Acrontum GmbH</small>
+      </div>
+      <div class="col-sm-6 col-lg-3 col-xl-2">
+        <h5 class="footer-title">Flying</h5>
+        <p><a href="javascript:void(0)">Lorem ipsum</a></p>
+        <p><a href="javascript:void(0)">dolor sit amet</a></p>
+        <p><a href="javascript:void(0)">adipisicing</a></p>
+        <p><a href="javascript:void(0)">consectur</a></p>
+      </div>
+      <div class="col-sm-6 col-lg-3 col-xl-2">
+        <h5 class="footer-title">Take-off/Landing</h5>
+        <p><a href="javascript:void(0)">Lorem ipsum</a></p>
+        <p><a href="javascript:void(0)">dolor sit amet</a></p>
+        <p><a href="javascript:void(0)">consectur</a></p>
+      </div>
+      <div class="col-sm-6 col-lg-3 col-xl-2">
+        <h5 class="footer-title">Parking</h5>
+        <p><a href="javascript:void(0)">Lorem ipsum</a></p>
+        <p><a href="javascript:void(0)">dolor sit amet</a></p>
+        <p><a href="javascript:void(0)">adipisicing</a></p>
+        <p><a href="javascript:void(0)">consectur</a></p>
+      </div>
+      <div class="col-sm-6 col-lg-3 col-xl-2">
+        <h5 class="footer-title">Destinations</h5>
+        <p><a href="javascript:void(0)">Lorem ipsum</a></p>
+        <p><a href="javascript:void(0)">dolor sit amet</a></p>
+        <p><a href="javascript:void(0)">consectur</a></p>
+      </div>
+      <div class="col-lg-6 col-xl-2">
+        <h5 class="footer-title">Contact us</h5>
+        <form>
+          <div class="form-group">
+            <input type="email" class="form-control" id="mail" placeholder="Your E-Mail">
+          </div>
+          <div class="form-group">
+            <input type="text" class="form-control" id="message" placeholder="Your Message">
+          </div>
+          <button type="submit" class="btn btn-secondary a-btn w-100">Send</button>
+        </form>
+      </div>
+    </div>
+
+    <hr>
+
+    <div class="row">
+      <div class="col-md-10 offset-md-2">
+        <h5 class="footer-notice"><a href="javascript:void(0)">Imprint and Legal Notice</a></h5>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+
+
+    mounted () {
+
+      let currentYear = new Date().getFullYear();
+
+      document.getElementById('year').innerHTML = currentYear;
+    }
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -8,18 +8,33 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/flying',
+    name: 'Flying',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "flying" */ '../views/Flying.vue')
+  },
+  {
+    path: '/takeoff-landing',
+    name: 'TOL',
+    component: () => import(/* webpackChunkName: "takeoff-landing" */ '../views/TOL.vue')
+  },
+  {
+    path: '/parking',
+    name: 'Parking',
+    component: () => import(/* webpackChunkName: "parking" */ '../views/Parking.vue')
+  },
+  {
+    path: '/destinations',
+    name: 'Destinations',
+    component: () => import(/* webpackChunkName: "Destinations" */ '../views/Destinations.vue')
   }
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
 
 export default router
